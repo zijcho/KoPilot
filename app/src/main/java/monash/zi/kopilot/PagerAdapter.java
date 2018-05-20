8705357e57b4838b8fa602e887d9dbb0ea52b39c
Zi Choong <zjcho1@student.monash.edu>
2018-05-20 20:17:59 +1000

    Initial for WIP1

---
+++ app/src/main/java/monash/zi/kopilot/PagerAdapter.java
@@ -0,0 +1,16 @@
+package monash.zi.kopilot;
+
+import android.view.View;
+
+public class PagerAdapter extends android.support.v4.view.PagerAdapter {
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return false;
+    }
+}
